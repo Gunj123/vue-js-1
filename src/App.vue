b350f2352eaa4823012fde2fabd4399574bc3fe4
gunj23 <gunjan.goswami.999@gmail.com>
2022-09-14 12:02:49 +0530

    commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <ul>
+    <li v-for="item in user" :key="item.name">
+      <user :data="item" :getdata="getdata"/>
+    </li>
+  </ul>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import user from './components/user.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    user
+  },
+  methods:{
+    getdata(name){
+      alert(name)
+    }
+  },
+  data() {
+    return {
+      user: [{ name: 'gunjan', email: 'gunjan.goswami.9@gmail.com' },
+      { name: 'sanjana', email: 'sanjana.bharadiya.9@gmail.com' },
+      { name: 'anil', email: 'anil.goswami.9@gmail.com' }
+      ]
+    }
+  },
+}
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/user.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="user">
+        <h2>{{data.name}}</h2>
+        <h2>{{data.email}}</h2>
+        <button v-on:click="getdata(data.name)">show name</button>
+    </div>
+
+</template>
+<script>
+export default {
+    name:'user-',
+    props:{
+        data:Object,
+        getdata:Function
+    },
+}
+</script>
+<style>
+  .user {
+    background-color: skyblue;
+    padding: 10px;
+    border: 1px solid black;
+    margin-bottom: 20px;
+} 
+</style>
